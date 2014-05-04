aecea67d5f469df12b6394def5e117ab4ef90cc8
labuser <labuser@lab-computer-28b68b.corp.adobe.com>
2014-05-04 01:45:35 -0700

    Added tab

---
+++ weiboTest/jokeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  jokeTableViewController.h
+//  weiboTest
+//
+//  Created by labuser on 2014/05/04.
+//  Copyright (c) 2014年 Haian Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface jokeTableViewController : UITableViewController
+
+@end
